a1189f6aafc9bccbeaf93ec5ee9fe4b24cf82441
Jonny Warren <jonnywarren06@gmail.com>
2015-09-29 17:44:50 -0400

    began writing javascript

---
+++ main.js
@@ -0,0 +1,30 @@
+//Grab Elements
+var clear = document.querySelector('#clear');
+var answer = document.querySelector('#answer');
+var seven = document.querySelector('#seven');
+var eight = document.querySelector('#eight');
+var nine = document.querySelector('#nine');
+var divide = document.querySelector('#divide');
+var four = document.querySelector('#four');
+var five = document.querySelector('#five');
+var six = document.querySelector('#six');
+var multiply = document.querySelector('#multiply');
+var one = document.querySelector('#one');
+var two = document.querySelector('#two');
+var three = document.querySelector('#three');
+var subtract = document.querySelector('#subtract');
+var zero = document.querySelector('#zero');
+var decimal = document.querySelector('#decimal');
+var equal = document.querySelector('#equal');
+var plus = document.querySelector('#plus');
+
+//Variable Declarations
+var answerText;
+
+//Function to Calculate and Generate Answer
+function answerMe() {
+	answerText = Number()
+}
+
+//Button Click
+calcBtn.addEventListener('click', answerMe);
